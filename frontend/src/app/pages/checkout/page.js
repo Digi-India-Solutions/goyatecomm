0b35e2f751562c68bec62dfec753b38a65f20d50
DIGI INDIA SOLUTIONS <digiindiasolutions2023@gmail.com>
2025-07-11 15:31:31 +0530

    changes

--- frontend/src/app/pages/checkout/page.js
+++ frontend/src/app/pages/checkout/page.js
@@ -409,7 +409,7 @@ export default function Page() {
                       </p>
                       <input
                         type="text"
-                        placeholder="Enter UPI ID"
+                        placeholder="Enter UTR ID"
                         className="mt-4 w-full px-4 py-2 border border-gray-400 rounded-md focus:ring-2 focus:ring-green-600 focus:outline-none text-black"
                         onChange={(e) =>
                           setFormData((prev) => ({
